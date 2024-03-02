245c2700d7752e089fe9e812fb2f5f8a397e714c
HarshitKr3005 <harshitkr3002@gmail.com>
2024-03-03 02:50:57 +0530

    completed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str)->List[str]:
 
 
 setup(
-    name = 'project',
+    name = 'Student Performance Indicator',
     version='0.0.1',
     author = 'Harshit',
     author_email='harshitkr3002@gmail.com',
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,5 @@
 import os
-import sys  
-sys.path.insert(0, '../src')
-import unittest
+import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
